1649f3c181011eed87be39a5b9a312396f505cf0
inAsees <gurasees.singh121@gmail.com>
2022-05-26 00:30:42 +0530

    class Scraper created which will scrap all the data. 1. Some instances created inside the constructor. 2. New method _get_total_pages created.

--- src/scrap.py
+++ src/scrap.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+from typing import Dict
+
+import requests as req
+from bs4 import BeautifulSoup as bs
 
 
 @dataclass
@@ -12,3 +16,19 @@ class ProductInfo:
     asin: str
     product_description: str
     manufacturer: str
+
+
+class Scraper:
+    def __init__(self, headers: Dict):
+        self._headers = headers
+        self._base_url = "https://www.amazon.in"
+        self._page_nav_url = "https://www.amazon.in/s?k=bags&page=1&crid=2M096C61O4MLT&qid=1653504203&sprefix=" \
+                             "ba%2Caps%2C283&ref=sr_pg_2"
+        self._total_pages = self._get_total_pages(self._page_nav_url)
+
+
+    def _get_total_pages(self, page_nav_url: str) -> int:
+        response = req.get(page_nav_url, headers=self._headers).text
+        response_soup = bs(response, "html.parser")
+
+        return int(response_soup.find("span", {"class": "s-pagination-item s-pagination-disabled"}).text)
