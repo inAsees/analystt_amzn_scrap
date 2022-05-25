ca97c100fafcea4d04170ea86443675ac99409a8
inAsees <gurasees.singh121@gmail.com>
2022-05-26 01:41:17 +0530

    Three new methods created as follows: 1. scrap_all_pages 2. _scrap_url 3. _parse_product_info

--- src/scrap.py
+++ src/scrap.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List
 
 import requests as req
 from bs4 import BeautifulSoup as bs
@@ -22,12 +22,32 @@ class Scraper:
     def __init__(self, headers: Dict):
         self._headers = headers
         self._base_url = "https://www.amazon.in"
-        self._page_nav_url = "https://www.amazon.in/s?k=bags&page=1&crid=2M096C61O4MLT&qid=1653504203&sprefix=" \
+        self._nav_page_url = "https://www.amazon.in/s?k=bags&page=1&crid=2M096C61O4MLT&qid=1653504203&sprefix=" \
                              "ba%2Caps%2C283&ref=sr_pg_2"
-        self._total_pages = self._get_total_pages(self._page_nav_url)
-        self._total_nav_page_urls_list = [
-            "https://www.amazon.in/s?k=bags&page={}&crid=2M096C61O4MLT&qid=1653504203&sprefix=" \
-            "ba%2Caps%2C283&ref=sr_pg_2".format(page_no) for page_no in range(1 , self._total_pages + 1)]
+        self._total_nav_pages = self._get_total_pages(self._nav_page_url)
+        self._total_nav_pages_url_list = ["https://www.amazon.in/s?k=bags&page={}&crid=2M096C61O4MLT&qid=" \
+                                          "1653504203&sprefix=ba%2Caps%2C283&ref=sr_pg_2".format(page_no) for page_no
+                                          in range(1, self._total_nav_pages + 1)]
+        self._product_info_list = []  # type: List[ProductInfo]
+
+    def scrap_all_pages(self) -> None:
+        page_no = 1
+        for url in self._total_nav_pages_url_list:
+            response = req.get(url, headers=self._headers).text
+            response_soup = bs(response, "html.parser")
+            self._scrap_url(response_soup, page_no)
+            page_no += 1
+
+    def _scrap_url(self, response_soup: bs, page_no: int) -> None:
+        result_set = response_soup.findAll("a", {"class": "a-link-normal s-underline-text s-underline-link-text s-"
+                                                          "link-style a-text-normal"})
+        for i in result_set:
+            url = self._base_url + i.get("href")
+            product_info = self._parse_product_info(url, response_soup)
+
+    @classmethod
+    def _parse_product_info(cls, url: str, response_soup: bs) -> ProductInfo:
+        product_url = url
 
     def _get_total_pages(self, page_nav_url: str) -> int:
         response = req.get(page_nav_url, headers=self._headers).text
