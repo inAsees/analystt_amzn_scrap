a0c1cbbc1c851574a3ef015ee52eefac7fd4fe4f
inAsees <gurasees.singh121@gmail.com>
2022-05-26 19:22:54 +0530

    New method inject_info_in_product_overview added.
    1. New method _get_description created.

--- src/scrap.py
+++ src/scrap.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, List, Optional
+from typing import Dict, Optional, List
 
 import requests as req
 from bs4 import BeautifulSoup as bs, Tag
@@ -28,24 +28,32 @@ class Scraper:
         self._total_nav_pages_url_list = ["https://www.amazon.in/s?k=bags&page={}&crid=2M096C61O4MLT&qid=" \
                                           "1653504203&sprefix=ba%2Caps%2C283&ref=sr_pg_{}".format(page_no, page_no) for
                                           page_no in range(1, self._total_nav_pages + 1)]
-        self._product_info_list = []  # type: List[ProductInfo]
+        self._product_info_list = []
 
     def scrap_all_pages(self) -> None:
-        for url in self._total_nav_pages_url_list:
+        for url in self._total_nav_pages_url_list[:1]:
             response = req.get(url, headers=self._headers).text
             response_soup = bs(response, "html.parser")
             self._parse_product_overview(response_soup)
 
+    def inject_info_in_product_overview(self) -> None:
+        for ele in self._product_info_list:
+            product_url = ele[0]
+            response = req.get(product_url, headers=self._headers).text
+            response_soup = bs(response, "html.parser")
+            product_description = self._get_description(response_soup)
+
+            print(product_description)
+
     def _parse_product_overview(self, response_soup: bs) -> None:
         r_set = response_soup.findAll("div", {"data-component-type": "s-search-result"})
         for tag in r_set:
-            product_name = self._get_product_name(tag)
             product_url = self._get_product_url(tag)
+            product_name = self._get_product_name(tag)
             product_price = self._get_product_price(tag)
             product_ratings = self._get_product_rating(tag)
             product_reviews = self._get_no_of_reviews(tag)
-
-            print(product_ratings, product_reviews)
+            self._product_info_list.append([product_url, product_name, product_price, product_ratings, product_reviews])
 
     def _get_product_url(self, tag: Tag) -> str:
         url = tag.find("a", {"class": "a-link-normal s-underline-text s-underline-link-text s-"
@@ -73,6 +81,16 @@ class Scraper:
             return tag.find("span", {"class": "a-size-base s-light-weight-text"}).text
         except AttributeError:
             return None
+
+    @staticmethod
+    def _get_description(response_soup: bs) -> List[List[str]]:
+        res = []
+        r_set = response_soup.find("div", {"id": "feature-bullets"}).findAll("li")
+        for txt in r_set:
+            refined_text = txt.text.strip().replace("\n", "")
+            res.append([refined_text])
+
+        return res
 
     def _get_total_pages(self, page_nav_url: str) -> int:
         response = req.get(page_nav_url, headers=self._headers).text
